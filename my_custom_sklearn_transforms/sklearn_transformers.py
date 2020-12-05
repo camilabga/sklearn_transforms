b43ae7cea3eef20204e621325e8f0d42a9882e9f
Camila Barbosa <camilagomss@gmail.com>
2020-12-05 14:42:02 -0300

    adjustments to final

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -15,7 +15,8 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
     
-class AdjustMeanSubject(BaseEstimator, TransformerMixin):
+    
+class FillNa(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
@@ -24,24 +25,15 @@ class AdjustMeanSubject(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         data = X.copy()
-        data['NOTA_GO'] = data.apply(
-            lambda row: ((row['NOTA_MF']+row['NOTA_EM']+row['NOTA_DE'])/3) if np.isnan(row['NOTA_GO']) else row['NOTA_GO'],axis=1
-        )
+        target0 = data[data['TARGET']==0]
+        target1 = data[data['TARGET']==1]
+        target2 = data[data['TARGET']==2]
+        target0['koi_pdisposition']=target0['koi_pdisposition'].fillna('CANDIDATE')
+        target1['koi_pdisposition']=target1['koi_pdisposition'].fillna('FALSE POSITIVE')
+        target2['koi_pdisposition']=target2['koi_pdisposition'].fillna('CANDIDATE')
         
-        return data
-
-class FeatureEngineering(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        df = X.copy()
-        df['REPROVACOES_H'] = df['REPROVACOES_DE'] + df['REPROVACOES_EM']
-        df['REPROVACOES_E'] = df['REPROVACOES_MF'] + df['REPROVACOES_GO']
-        df['MEDIA_H'] = (df['NOTA_DE']+df['NOTA_EM'])/2
-        df['MEDIA_E'] = (df['NOTA_MF']+df['NOTA_GO'])/2
-
-        return df
+        data0 = pd.concat([target0, target1, target2]) 
+        
+        return data0
+    
+    
